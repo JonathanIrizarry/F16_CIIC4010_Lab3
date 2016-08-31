23f91145f1851c79327f78d8879ff718f11d86e8
4015a8 <4015a8@chironja.ece.uprm.edu>
2016-08-31 08:07:00 -0400

    JFrame name changed to my own.
    Window opens in center.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Jonathan Irizarry Quintero");
+		JFrame myFrame = new JFrame("Jonathan A. Irizarry Quintero");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(900, 450);
 		myFrame.setSize(200, 200);
